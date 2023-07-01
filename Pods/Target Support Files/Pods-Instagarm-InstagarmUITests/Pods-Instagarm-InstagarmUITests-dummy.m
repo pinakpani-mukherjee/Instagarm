d48d2b958d45cac30ae6434fa42cc4f765c80a64
pinakpani-mukherjee <43544655+pinakpani-mukherjee@users.noreply.github.com>
2023-07-01 11:46:06 +0900

    Delete Pods directory

--- Pods/Target Support Files/Pods-Instagarm-InstagarmUITests/Pods-Instagarm-InstagarmUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Instagarm_InstagarmUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Instagarm_InstagarmUITests
-@end
--- Pods/Target Support Files/Pods-Instagarm/Pods-Instagarm-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Instagarm : NSObject
-@end
-@implementation PodsDummy_Pods_Instagarm
-@end
